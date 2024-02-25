f647f6c136e289019756a330bc82b742eda443da
rcaselles <rcaselles98@gmail.com>
2024-02-25 21:25:59 +0100

    Header fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,87 @@
-"use client"
-import { useState } from 'react';
-import ThemeChanger from '../ThemeChanger';
-import Link from "next/link";
-import { usePathname } from 'next/navigation';
+'use client'
+import { useState } from 'react'
+import ThemeChanger from '../ThemeChanger'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const currentRoute = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const currentRoute = usePathname()
 
   const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
 
-  const linkClass = (path) => {
+  const linkClass = path => {
     return `block py-2 px-3 md:p-0 rounded md:bg-transparent hover:font-bold hover:tracking-normal ${
       currentRoute === path
-        ? "font-bold bg-blue-700 md:text-gray-900 md:dark:text-gray-500"
-        : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 md:dark:hover:text-gray-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-    }`;
-  };
+        ? 'font-bold bg-gray-300 md:text-gray-900 md:dark:text-gray-500'
+        : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 md:dark:hover:text-gray-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+    }`
+  }
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://rcaselles.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">rcaselles</span>
+        <a
+          href="https://rcaselles.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            rcaselles
+          </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <ThemeChanger />
-          <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="true">
+          <button
+            onClick={toggleMobileMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            aria-expanded="true"
+          >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
             </svg>
           </button>
         </div>
-        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="navbar-cta">
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isMobileMenuOpen ? 'block' : 'hidden'
+          }`}
+          id="navbar-cta"
+        >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link href="/" className={linkClass("/")}>
+              <Link href="/" className={linkClass('/')}>
                 Home
               </Link>
             </li>
             <li>
-              <Link href="/posts" className={linkClass("/posts")}>
+              <Link href="/posts" className={linkClass('/posts')}>
                 Posts
               </Link>
             </li>
             <li>
-              <Link href="/projects" className={linkClass("/projects")}>
+              <Link href="/projects" className={linkClass('/projects')}>
                 Projects
               </Link>
             </li>
             <li>
-              <Link href="/about" className={linkClass("/about")}>
+              <Link href="/about" className={linkClass('/about')}>
                 About
               </Link>
             </li>
@@ -64,4 +92,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default Header
--- src/sections/IntroSection.jsx
+++ src/sections/IntroSection.jsx
@@ -4,11 +4,7 @@ const IntroSection = ({ heading, subHeading, socials }) => {
   return (
     <section className="w-full text-center items-center justify-center flex flex-col-reverse md:flex-row">
       <div className="flex-1 flex flex-col gap-y-4 text-center items-center justify-center">
-        <h1
-          className="text-3xl md:text-5xl font-bold max-w-2xl text-fore-primary relative w-[max-content] 
-                      before:absolute before:inset-0 before:animate-typewriter
-                      after:absolute after:inset-0 after:w-[0.125em] after:animate-caret"
-        >
+        <h1 className="text-3xl md:text-5xl font-bold max-w-2xl text-fore-primary relative ">
           {heading || 'Developer Portfolio'}
         </h1>
         <h2 className="mb-4 max-w-lg">{subHeading || ''}</h2>
